baf514b142884bcb40c32f0533b08c35c5cb1737
Georgy Teltevsky <axel_23@list.ru>
2019-08-05 18:20:08 +0300

    codeformatting

--- sandbox/src/main/java/ru/st/j4qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/st/j4qa/sandbox/MyFirstProgram.java
@@ -1,12 +1,12 @@
 package ru.st.j4qa.sandbox;
 
 public class MyFirstProgram {
-	
-	public static void main(String[] args) {
-	
-		System.out.println("Hello, world!");
-	
-	}
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+    }
 
 }
 
