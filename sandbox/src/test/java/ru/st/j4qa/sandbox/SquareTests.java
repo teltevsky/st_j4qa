dfa1b027fede52cd4ca93b0d728e4ebc6fad6813
Georgy Teltevsky <axel_23@list.ru>
2019-08-07 18:26:01 +0300

    Второй урок: подключение фреймворка testng

---
+++ sandbox/src/test/java/ru/st/j4qa/sandbox/SquareTests.java
@@ -0,0 +1,13 @@
+package ru.st.j4qa.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+    @Test
+    public void testArea() {
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25);
+    }
+}
