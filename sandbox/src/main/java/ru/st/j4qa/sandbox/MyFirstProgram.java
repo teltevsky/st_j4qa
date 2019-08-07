7414091f76e83777127f5e99707768e14273ede5
Georgy Teltevsky <axel_23@list.ru>
2019-08-07 11:15:55 +0300

    добавлено несколько функций

--- sandbox/src/main/java/ru/st/j4qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/st/j4qa/sandbox/MyFirstProgram.java
@@ -3,10 +3,29 @@ package ru.st.j4qa.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Feofan");
 
-        System.out.println("Hello, world!");
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b +  " равна " + area(a, b));
 
     }
 
+    public static void hello(String somebody)   {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len)   {
+        return len * len;
+    }
+
+    public static double area(double a, double b)   {
+        return a * b;
+    }
 }
 
