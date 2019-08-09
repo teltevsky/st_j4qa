ffdd09ce9e60de2ca9165a7b9dd8861d20260377
Georgy Teltevsky <axel_23@list.ru>
2019-08-09 12:40:16 +0300

    Доработал второе задание
    удалил класс TwoPoints, перенёс функцию в Point

--- sandbox/src/main/java/ru/st/j4qa/sandbox/DistancePointsPlane.java
+++ sandbox/src/main/java/ru/st/j4qa/sandbox/DistancePointsPlane.java
@@ -7,18 +7,8 @@ public class DistancePointsPlane {
 
         Point p1 = new Point(1, 3);
         Point p2 = new Point(6, 7);
-        System.out.println("Расстояние между двумя точками, лежащими в одной плоскости, с координатами [" + p1.x + "; " + p1.y + "] и [" + p2.x + "; " + p2.y + "] равно " + distance(p1, p2));
+        System.out.println("Расстояние между двумя точками, лежащими в одной плоскости, с координатами [" + p1.x + "; " + p1.y + "] и [" + p2.x + "; " + p2.y + "] равно " + p1.distance(p2));
 
-
-//  Не сообразил, как при переносе функции (метода) в класс Point указать разные параметры (координаты) для разных точек (объектов).
-//  Создал новый класс TwoPoints
-        TwoPoints tp = new TwoPoints(5,12,-7,15);
-        System.out.println("NEW Расстояние между двумя точками, лежащими в одной плоскости, с координатами [" + tp.x1 + "; " + tp.y1 + "] и [" + tp.x2 + "; " + tp.y2 + "] равно " + tp.distance());
-
-    }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
     }
 
 }
--- sandbox/src/main/java/ru/st/j4qa/sandbox/Point.java
+++ sandbox/src/main/java/ru/st/j4qa/sandbox/Point.java
@@ -9,6 +9,11 @@ public class Point{
     public Point(double x, double y)   {
         this.x = x;
         this.y = y;
+
     }
 
+    public  double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+
+    }
 }
